f7db66129253bd0d23314ebfffda1b00af09e72b
Sai Kasi <prabhavkasibhatla@gmail.com>
2024-03-21 22:40:19 -0500

    Basic Model Scafolding

--- RNN_ Apparatus/model.py
+++ RNN_ Apparatus/model.py
@@ -1,1 +1,44 @@
 # Just the model nothing else
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import os
+
+# Recursive Nerual Net Model
+class RNN_Model(nn.Module):
+
+    # Instation
+    def __init__(self):
+        super().__init__()
+        # TODO
+        pass
+
+    #  Basic forward operation #TODO ask Darth if this is alr
+    def forward(self, x):
+            x = F.relu(self.linear1(x))
+            x = self.linear2(x)
+            return x
+
+    # Save data of training into a pth file (helpful to show training progression) : for memmory
+    def save(self, file_name='model.pth'):
+        model_folder_path = './model'
+        if not os.path.exists(model_folder_path):
+            os.makedirs(model_folder_path)
+
+        file_name = os.path.join(model_folder_path, file_name)
+        torch.save(self.state_dict(), file_name)
+
+# Model trainer
+class RNNTranier:
+    # Basic innit subject to change
+    def __init__(self, model, lr, gamma):
+        self.lr = lr # learning rate
+        self.gamma = gamma # influence of single training exmaple
+        self.model = model
+        self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
+        self.criterion = nn.MSELoss() # Mean Squared Regression Criterion
+
+    # TODO
+    def train_step(self):
+        pass
