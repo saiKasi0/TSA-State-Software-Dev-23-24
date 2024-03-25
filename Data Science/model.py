9fd02273fd5ea4612aa7f18dc32a73d3d62a2958
Sai Kasi <prabhavkasibhatla@gmail.com>
2024-03-24 20:35:47 -0500

    LSTM Done w/ proof of concept

--- Data Science/model.py
+++ Data Science/model.py
@@ -1,69 +1,23 @@
-import torch
 import torch.nn as nn
-import torch.functional as F
 from torch.optim import Adam
-
 import lightning as L
-from torch.utils.data import TensorDataset, DataLoader
 
-class LSTMbyHand(L.LightningModule):
-    
-    # Create & Init Weight and Bias Tensors
-    def __init__(self):
-
+class LightningLSTM(L.LightningModule):
+    def __init__(self) -> None:
         super().__init__()
-        mean  = torch.tensor(0.0)
-        std =  torch.tensor(1.0)
+        self.lstm = nn.LSTM(input_size=1, hidden_size=1)
 
-        #  Initially random weights and biases for LSTM 
-        self.wlr1 = nn.Parameter(torch.normal(mean=mean, std=std), requires_grad=True)
-        self.wlr2 = nn.Parameter(torch.normal(mean=mean, std=std), requires_grad=True)
-        self.blr1 = nn.Parameter(torch.tensor(0.), requires_grad=True)
-
-        self.wpr1 = nn.Parameter(torch.normal(mean=mean, std=std), requires_grad=True)
-        self.wpr2 = nn.Parameter(torch.normal(mean=mean, std=std), requires_grad=True)
-        self.bpr1 = nn.Parameter(torch.tensor(0.), requires_grad=True)
-
-        self.wp1 = nn.Parameter(torch.normal(mean=mean, std=std), requires_grad=True)
-        self.wp2 = nn.Parameter(torch.normal(mean=mean, std=std), requires_grad=True)
-        self.bp1 = nn.Parameter(torch.tensor(0.), requires_grad=True)
-
-        self.wo1 = nn.Parameter(torch.normal(mean=mean, std=std), requires_grad=True)
-        self.wo2 = nn.Parameter(torch.normal(mean=mean, std=std), requires_grad=True)
-        self.bo1 = nn.Parameter(torch.tensor(0.), requires_grad=True)
-
-    # Handle LSTM Operations
-    def lstm_unit(self, input_value, long_memory, short_memory):
-        # Forget Gate: Calculates How Much of Long Term Memory to Implement
-        long_remember_percent = torch.sigmoid((short_memory * self.wlr1) + (input_value * self.wlr2) + self.blr1)
-
-        # Input Gate: Creates New Potential Long-term Memory and Determines How Much To Remember
-        potential_remember_percent = torch.sigmoid((short_memory * self.wpr1) + (input_value * self.wpr2) + self.bpr1)
-        potential_memory = torch.tanh((short_memory * self.wp1) + (input_value * self.wp2) + self.bp1)
-
-        # Updates Long Term Memory
-        updated_long_memory = ((long_memory * long_remember_percent) + (potential_remember_percent * potential_memory))
-        
-        # Output Gate: Makes New Short Term Memory and Determines How Much To Remember
-        output_percent = torch.sigmoid((short_memory * self.wo1) + (input_value * self.wo2) + self.bo1)
-        updated_short_memory =  torch.tanh(updated_long_memory) * output_percent
-
-        return ([updated_long_memory, updated_short_memory])
-
-    # Conducts Forward Pass in Unrolled LSTM
     def forward(self, input):
-        long_memory = 0
-        short_memory = 0
-        for data in input:
-            long_memory, short_memory = self.lstm_unit(data, long_memory, short_memory)
-        return short_memory
+        input_trans = input.view(len(input), 1)
+        lstm_out, temp  = self.lstm(input_trans)
 
-    # Configures Adam Optimizer
+        prediction = lstm_out[-1]
+        return prediction 
+    
     def configure_optimizers(self):
-        return Adam(self.parameters)
-
-    # Calculate Loss (Sum of Squared Residuals) and Log Training Progress
-    def training_step(self, batch, batch_idx):
+        return Adam(self.parameters(), lr = 0.1)
+    
+    def training_step(self, batch):
         input_i, label_i = batch
         output_i =  self.forward(input_i[0])
         loss = (output_i - label_i)**2 # Sum of Squared Residuals
