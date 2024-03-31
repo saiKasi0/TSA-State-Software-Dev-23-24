6c34acf916782ba95268e2e7c94d8fc0df5514a3
Sai Kasi <prabhavkasibhatla@gmail.com>
2024-03-30 23:12:10 -0500

    Model!!!

--- Data Science/model.py
+++ Data Science/model.py
@@ -1,26 +1,44 @@
 import torch.nn as nn
 from torch.optim import Adam
 import lightning as L
+import torch
 
-class LightningLSTM(L.LightningModule):
-    def __init__(self) -> None:
-        super().__init__()
-        self.lstm = nn.LSTM(input_size=1, hidden_size=1)
+input_dim = 1
+hidden_dim = 32
+num_layers = 2 
+output_dim = 1
 
-    def forward(self, input):
-        input_trans = input.view(len(input), 1)
-        lstm_out, temp  = self.lstm(input_trans)
+class LSTM(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_layers, output_dim):
+        super(LSTM, self).__init__()
+        # Hidden dimensions
+        self.hidden_dim = hidden_dim
 
-        prediction = lstm_out[-1]
-        return prediction 
+        # Number of hidden layers
+        self.num_layers = num_layers
+
+        # batch_first=True causes input/output tensors to be of shape
+        # (batch_dim, seq_dim, feature_dim)
+        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True)
+
+        # Readout layer
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        # Initialize hidden state with zeros
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
+
+        # Initialize cell state
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
+
+        # We need to detach as we are doing truncated backpropagation through time (BPTT)
+        # If we don't, we'll backprop all the way to the start even after going through another batch
+        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+
+        # Index hidden state of last time step
+        # out.size() --> 100, 32, 100
+        # out[:, -1, :] --> 100, 100 --> just want last time step hidden states! 
+        out = self.fc(out[:, -1, :]) 
+        # out.size() --> 100, 10
+        return out
     
-    def configure_optimizers(self):
-        return Adam(self.parameters(), lr = 0.1)
-    
-    def training_step(self, batch):
-        input_i, label_i = batch
-        output_i =  self.forward(input_i[0])
-        loss = (output_i - label_i)**2 # Sum of Squared Residuals
-        self.log("train_loss", loss)
-
-        return loss
